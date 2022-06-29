4e5fe8f176173514b8f3e302874aae67f1f28525
Ilnur-web <ilmingaleev@yandex.ru>
2022-06-29 11:08:18 +0300

    Added Joystick
    Joystick from Unity assetstore

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    private Rigidbody2D rigidBody2D;
     private float moveH, moveV;
     [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private Joystick joystick;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        moveH = Input.GetAxis("Horizontal") * moveSpeed;
-        moveV = Input.GetAxis("Vertical") * moveSpeed;
-        rb.velocity = new Vector2(moveH, moveV);//OPTIONAL rb.MovePosition();
+        moveH = JoystickInput(joystick.Horizontal);
+        moveV = JoystickInput(joystick.Vertical);
+        rigidBody2D.velocity = new Vector2(moveH, moveV);//OPTIONAL rb.MovePosition();
 
         Vector2 direction = new Vector2(moveH, moveV);
 
         FindObjectOfType<PlayerAnimation>().SetDirection(direction);
+    }
+    private float JoystickInput(float axis) 
+    {
+        if (axis >= .2f)
+        {
+           return moveSpeed;
+        }
+        else if (axis <= -.2f)
+        {
+           return -moveSpeed;
+        }
+        else
+        {
+            return  0;
+        }
+
     }
 
 
